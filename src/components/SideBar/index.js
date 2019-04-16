015ed6266dbc928c8ee537c406d116219e4145ee
Jean Cabral <jeancabral@gmail.com>
2019-04-16 11:06:55 -0300

    Adiconando Styled Components e SideBar Component

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+import { Container } from './styles';
+
+export default class SideBar extends Component {
+  //   static propTypes = {
+  //     prop: PropTypes,
+  //   };
+
+  render() {
+    return <Container />;
+  }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,14 @@ import React, { Fragment } from 'react';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from '../pages/main';
+import SideBar from '../components/SideBar';
+import GlobalStyle from '../styles/global';
 
 const Routes = () => (
   <BrowserRouter>
     <Fragment>
+      <GlobalStyle />
+      <SideBar />
       <Switch>
         <Route path="/" component={Main} />
       </Switch>
